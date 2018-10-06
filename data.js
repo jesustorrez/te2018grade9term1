38e7a4c7c75fac7b4bdc2fd780778a9b68b9e83f
Jesus <jesustcsegde.org>
2018-10-06 13:18:47 -0700

    updated to data.js
    updated to data.js

--- data.js
+++ data.js
@@ -1,5 +1,19 @@
 // Example
-let message = "Hello World!";
+let message = "Hello chike!";
 console.log(message);
+let bDayMonth=1
+let bDay=16
+let age=15
+let siblings=1
+let people= "help me"
+let fortnite
 
-// Type your code below!
+
+
+
+
+
+
+
+
+// Type your code below
