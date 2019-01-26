d218fe165df5829bf260d59281e4c63c8cfd6fd3
Jesus Torres <jesust@csedge.org>
2019-01-26 10:51:21 -0800

    website
    website

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # teamedge2018
+
+<!DOCTYPE html>
+
+
+  <head>
+    <title>Jt's website</title>
+  </head>
+
+
+  <body>
+  This is my website.
+  <a href="https://media.giphy.com/media/boj>
+  </body>
+
+
+</html>
--- scratchpad.js
+++ scratchpad.js
@@ -1,1 +1,8 @@
-// Use this script as a sandbox to test things!
+let dog ={};
+dog.color = 'brown';
+dog.numlegs = 4;
+dog.breed = 'golden retriever';
+dog.bark = function () {
+    return ('roof roof')
+}
+console.log(dog.bark());
